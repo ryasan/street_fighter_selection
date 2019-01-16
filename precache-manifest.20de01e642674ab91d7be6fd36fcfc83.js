b834ad5909938d9707bea826e51768a4ca9965c0
Ryan Santos <ryansantos86@gmail.com>
2019-01-15 17:23:39 -0800

    Updates

---
+++ precache-manifest.20de01e642674ab91d7be6fd36fcfc83.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a377c508334a10ce7d4d",
+    "url": "/street_fighter_selection/static/css/main.266700f4.chunk.css"
+  },
+  {
+    "revision": "a377c508334a10ce7d4d",
+    "url": "/street_fighter_selection/static/js/main.a377c508.chunk.js"
+  },
+  {
+    "revision": "17dff291dae384d7bc7a",
+    "url": "/street_fighter_selection/static/js/1.17dff291.chunk.js"
+  },
+  {
+    "revision": "83a2785086eb9c8ecd6e",
+    "url": "/street_fighter_selection/static/js/runtime~main.83a27850.js"
+  },
+  {
+    "revision": "096ecd3ba8a927ac3cb77b7aebd9b5e7",
+    "url": "/street_fighter_selection/index.html"
+  }
+];
