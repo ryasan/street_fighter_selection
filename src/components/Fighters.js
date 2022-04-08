2b0eabe3f167d09a5258039a313d854eda3c21a4
ryasan86 <ryansantos86@gmail.com>
2022-04-08 13:29:17 -0700

    Refactor constants

--- src/components/Fighters.js
+++ src/components/Fighters.js
@@ -1,59 +1,87 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from 'react'
+import styled from 'styled-components'
 
-import {Cell, Grid, Img} from './common';
-import {portraitIds, FIGHTER_GRID_WIDTH} from './../constants';
-import {slideIn} from './../utils/slideInAnimation';
-import Player1 from './Player1';
+import {Cell, Grid, Img} from './common'
+import {slideIn} from '../utils/slideInAnimation'
+import {
+	Balrog,
+  Blanka,
+  Ryu,
+  Ehonda,
+  Guile,
+  Ken,
+  Mbison,
+  Sagat,
+  Chunli,
+  Dhalsim,
+  Vega,
+  Zangief,
+} from '../images'
+import Player1 from './Player1'
+
+const portraitIds = {
+  'Ryu': Ryu,
+  'E.Honda': Ehonda,
+  'Blanka': Blanka,
+  'Guile': Guile,
+  'Balrog': Balrog,
+  'Vega': Vega,
+  'Ken': Ken,
+  'Chun Li': Chunli,
+  'Zangief': Zangief,
+  'Dhalsim': Dhalsim,
+  'Sagat': Sagat,
+  'M.Bison': Mbison,
+}
 
 const FighterGrid = styled(Grid)`
-	grid-area: fighters;
-	width: ${FIGHTER_GRID_WIDTH}px;
-	outline: none;
-	display: grid;
-	grid-template-columns: repeat(6, ${FIGHTER_GRID_WIDTH / 6}px);
-	border: 2px solid ${({theme}) => theme.color.darkGray};
-	right: -100vw;
-	position: relative;
-	animation: ${slideIn('right')} 1s forwards;
-`;
+  grid-area: fighters;
+  width: 720px;
+  outline: none;
+  display: grid;
+  grid-template-columns: repeat(6, ${720 / 6}px);
+  border: 2px solid ${({theme}) => theme.color.darkGray};
+  right: -100vw;
+  position: relative;
+  animation: ${slideIn('right')} 1s forwards;
+`
 
 const FighterCell = styled(Cell)`
-	border: 2px solid ${({theme}) => theme.color.darkGray};
-	outline: none;
-	display: flex;
-	flex-direction: column;
-	justify-content: space-between;
-	align-items: center;
-`;
+  border: 2px solid ${({theme}) => theme.color.darkGray};
+  outline: none;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+`
 
 const FighterLabel = styled.label`
-	height: 1.5em;
-	color: white;
-	background: ${({theme}) => theme.color.primary};
-	font-size: 12px;
-	margin-top: -2em;
-	width: 100%;
-	text-align: center;
-	justify-content: center;
-`;
+  height: 1.5em;
+  color: white;
+  background: ${({theme}) => theme.color.primary};
+  font-size: 12px;
+  margin-top: -2em;
+  width: 100%;
+  text-align: center;
+  justify-content: center;
+`
 
 export default function Fighters(props) {
-	const renderFighterCells = () => {
-		const {active, fighterNames} = props;
+  const renderFighterCells = () => {
+    const {active, fighterNames} = props
 
-		return [].concat(...fighterNames).map((fighter, i) => {
-			return (
-				<FighterCell key={i} onKeyPress={props.handleKeyPress} active={active === fighter}>
-					{active === fighter ? <Player1 /> : ''}
+    return [].concat(...fighterNames).map((fighter, i) => {
+      return (
+        <FighterCell key={i} onKeyPress={props.handleKeyPress} active={active === fighter}>
+          {active === fighter ? <Player1 /> : ''}
 
-					<Img src={portraitIds[fighter]} alt={fighter} />
+          <Img src={portraitIds[fighter]} alt={fighter} />
 
-					{active === fighter ? <FighterLabel>{fighter}</FighterLabel> : ''}
-				</FighterCell>
-			);
-		});
-	};
+          {active === fighter ? <FighterLabel>{fighter}</FighterLabel> : ''}
+        </FighterCell>
+      )
+    })
+  }
 
-	return <FighterGrid onKeyDown={props.handleKeyPress}>{renderFighterCells()}</FighterGrid>;
+  return <FighterGrid onKeyDown={props.handleKeyPress}>{renderFighterCells()}</FighterGrid>
 }
--- src/components/WorldMap.js
+++ src/components/WorldMap.js
@@ -1,75 +1,149 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from 'react'
+import styled from 'styled-components'
 
-import {Cell, Grid} from './common';
-import {slideIn} from './../utils/slideInAnimation';
-import {countries} from './../constants';
-import {World} from './../images';
+import {Cell, Grid} from './common'
+import {slideIn} from './../utils/slideInAnimation'
+import {World} from './../images'
+import {USA, USSR, China, Japan, Brazil, Thailand, India, Spain} from '../images'
 
-const MAP_ROWS = 8;
-const MAP_COLUMNS = 12;
+const MAP_ROWS = 8
+const MAP_COLUMNS = 12
+
+const countries = [
+  {
+    name: 'USSR',
+    flag: USSR,
+    fighter: 'Zangief',
+    coordinates: [0, 7],
+  },
+  {
+    name: 'USA',
+    flag: USA,
+    fighter: 'Ken',
+    coordinates: [1, 2],
+  },
+  {
+    name: 'SPAIN',
+    flag: Spain,
+    fighter: 'Vega',
+    coordinates: [1, 4],
+  },
+  {
+    name: 'JAPAN',
+    flag: Japan,
+    fighter: 'Ryu',
+    coordinates: [1, 9],
+  },
+  {
+    name: 'USA',
+    flag: USA,
+    fighter: 'Guile',
+    coordinates: [2, 1],
+  },
+  {
+    name: 'USA',
+    flag: USA,
+    fighter: 'Balrog',
+    coordinates: [2, 2],
+  },
+  {
+    name: 'CHINA',
+    flag: China,
+    fighter: 'Chun Li',
+    coordinates: [2, 8],
+  },
+  {
+    name: 'JAPAN',
+    flag: Japan,
+    fighter: 'E.Honda',
+    coordinates: [2, 9],
+  },
+  {
+    name: 'INDIA',
+    flag: India,
+    fighter: 'Dhalsim',
+    coordinates: [3, 7],
+  },
+  {
+    name: 'THAILAND',
+    flag: Thailand,
+    fighter: 'M.Bison',
+    coordinates: [4, 8],
+  },
+  {
+    name: 'THAILAND',
+    flag: Thailand,
+    fighter: 'Sagat',
+    coordinates: [4, 9],
+  },
+  {
+    name: 'BRAZIL',
+    flag: Brazil,
+    fighter: 'Blanka',
+    coordinates: [5, 3],
+  },
+]
 
 const MapGrid = styled(Grid)`
-	grid-area: world-map;
-	width: 1000px;
-	height: 400px;
-	background: url(${World}) center center no-repeat;
-	background-size: contain;
-	grid-template-columns: repeat(${MAP_COLUMNS}, 1fr);
-	grid-template-rows: repeat(${MAP_ROWS}, 1fr);
-	top: -100vh;
-	position: relative;
-	animation: ${slideIn('top')} 1s forwards;
-`;
+  animation: ${slideIn('top')} 1s forwards;
+  background-size: contain;
+  background: url(${World}) center center no-repeat;
+  grid-area: world-map;
+  grid-template-columns: repeat(${MAP_COLUMNS}, 1fr);
+  grid-template-rows: repeat(${MAP_ROWS}, 1fr);
+  height: 400px;
+  position: relative;
+  top: -100vh;
+  width: 1000px;
+`
 
 const CellWrapper = styled.div`
-	text-align: center;
-	color: ${({active, theme}) => (active ? theme.color.secondary : 'white')};
-	font-size: 12px;
-	font-weight: bolder;
-	display: flex;
-	flex-direction: column;
-	padding: 0 10px;
-`;
+  color: ${({active, theme}) => (active ? theme.color.secondary : 'white')};
+  display: flex;
+  flex-direction: column;
+  font-size: 12px;
+  font-weight: bolder;
+  padding: 0 10px;
+  text-align: center;
+`
 
 const Flag = styled(Cell)`
-	background: url(${({countryBg}) => countryBg}) no-repeat center center;
-	background-size: cover;
-	border: none;
-	flex: 1;
-	box-shadow: ${({active, theme}) =>
-		active ? `inset 0px 0px 0px 2px ${theme.color.secondary};` : 'none'};
-`;
+  background-size: cover;
+  background: url(${({countryBg}) => countryBg}) no-repeat center center;
+  border: none;
+  box-shadow: ${({active, theme}) => active ? `inset 0px 0px 0px 2px ${theme.color.secondary};` : 'none'};
+  flex: 1;
+`
 
 const Text = styled.div`
-	font-size: 10px;
-`;
+  font-size: 10px;
+`
 
 function MapCell({country, selectedFighter}) {
-	return (
-		<CellWrapper>
-			{country && (
-				<>
-					<Flag countryBg={country.flag} active={country.fighter === selectedFighter} />{' '}
-					<Text>{country.name}</Text>
-				</>
-			)}
-		</CellWrapper>
-	);
+  return (
+    <CellWrapper>
+      {country && (
+        <>
+          <Flag countryBg={country.flag} active={country.fighter === selectedFighter} />{' '}
+          <Text>{country.name}</Text>
+        </>
+      )}
+    </CellWrapper>
+  )
 }
 
 export default function WorldMap({selectedFighter}) {
-	return (
-		<MapGrid>
-			{Array.from({length: MAP_ROWS}, (_, i) =>
-				Array.from({length: MAP_COLUMNS}, (_, ii) => (
-					<MapCell
-						key={ii}
-						country={countries.find((c) => c.coordinates[0] === i && c.coordinates[1] === ii)}
-						selectedFighter={selectedFighter}
-					/>
-				)),
-			)}
-		</MapGrid>
-	);
+  return (
+    <MapGrid>
+      {Array.from({length: MAP_ROWS}, (_, i) =>
+        Array.from({length: MAP_COLUMNS}, (_, ii) => (
+          <MapCell
+            key={ii}
+            country={countries.find((c) => c.coordinates[0] === i && c.coordinates[1] === ii)}
+            selectedFighter={selectedFighter}
+          />
+        )),
+      )}
+    </MapGrid>
+  )
 }
